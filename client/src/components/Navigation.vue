7d053c2fdacb5e0ec2b66d24bea23389a93025e7
Aqlan Nor Azman <aqlanhadi@gmail.com>
2020-10-18 01:52:29 +0800

    init

---
+++ client/src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="component p-5 text-center divide-y">
+        <!-- <span class="p">P</span> -->
+        <span class="perenggan text-3xl" aria-label="perenggan-logo">Perenggan</span>
+        <!-- <p class="text-3xl">{{ new Date().getFullDate() }}</p> -->
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Linden+Hill&display=swap');
+
+.component {
+    color: theme('colors.brand');
+    font-family: 'Linden Hill', serif;
+}
+</style>
